10b1c874b12f52d4cd9ec5780346724834d52794
Varya Medvedeva <146765645+Kkkkkk18@users.noreply.github.com>
2024-04-23 23:54:28 +0300

    add create,calculation matrix

---
+++ main.cpp
@@ -0,0 +1,67 @@
+﻿
+#include <iostream>
+#include <random>
+#include <chrono>
+
+
+const int MATRIX_R = 500;
+const int MATRIX_C = 500;
+
+void createRandomMatrix(int matrix[MATRIX_R][MATRIX_C], int row, int column) {
+
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < column; ++j) {
+            matrix[i][j] = std::rand() % 100 + 1;
+        }
+    }
+}
+
+void calculationRSum(const int matrix[MATRIX_R][MATRIX_C], int row, int column) {
+
+    int row_total_sum = 0;
+    for (int i = 0; i < row; ++i) {
+        int row_sum = 0;
+        for (int j = 0; j < column; ++j) {
+            row_sum += matrix[i][j];
+            row_total_sum += matrix[i][j];
+        }
+       
+    }
+    
+}
+
+void calculationCSum(const int matrix[MATRIX_R][MATRIX_C], int row, int column) {
+
+    int col_total_sum = 0;
+    for (int j = 0; j < column; ++j) {
+        int col_sum = 0;
+        for (int i = 0; i < row; ++i) {
+            col_sum += matrix[i][j];
+            col_total_sum += matrix[i][j];
+        }
+    }
+
+}
+int main() {
+    
+    const int row = 500;
+    const int column = 500;
+
+    int matrix[MATRIX_R][MATRIX_C];
+
+    auto r_start = std::chrono::high_resolution_clock::now();
+    calculationRSum(matrix, row, column);
+    auto r_end = std::chrono::high_resolution_clock::now();
+    auto row_duration = std::chrono::duration<double, std::milli>(r_end - r_start);
+    std::cout << "time spend by rows: " << row_duration.count() << "ms" << std::endl;
+
+
+    auto c_start = std::chrono::high_resolution_clock::now();
+    calculationCSum(matrix, row, column);
+    auto c_end = std::chrono::high_resolution_clock::now();
+    auto col_duration = std::chrono::duration<double, std::milli>(c_end - c_start);
+    std::cout << "time spend by columns: " << col_duration.count() << "ms" << std::endl;
+
+    
+    return 0;
+}
